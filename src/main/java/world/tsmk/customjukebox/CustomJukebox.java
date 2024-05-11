c5e934b70f163952f74497aec48082e834858018
scattered_leaves <xiaomoshuiya@qq.com>
2024-05-12 01:29:10 +0800

    初始

---
+++ src/main/java/world/tsmk/customjukebox/CustomJukebox.java
@@ -0,0 +1,10 @@
+package world.tsmk.customjukebox;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CustomJukebox implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/world/tsmk/customjukebox/client/CustomJukeboxClient.java
@@ -0,0 +1,10 @@
+package world.tsmk.customjukebox.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomJukeboxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
